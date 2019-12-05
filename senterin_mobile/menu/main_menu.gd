708aca297d947384f43e5b4e92bc89178ec2b902
Pyrozen <william.oky@binus.ac.id>
2019-12-05 17:10:42 +0700

    Add Fade In-Fade Out Transition

--- senterin_mobile/menu/main_menu.gd
+++ senterin_mobile/menu/main_menu.gd
@@ -1,31 +1,24 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 var scene_path_to_load
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	#$Menu/CenterRow/Buttons/NewGameButton.grab_focus()
-#	$FadeOut.show()
-#	$FadeOut.fade_out()
+	$FadeOut.show()
+	$FadeOut.fade_out()
 	for button in $menu/center_row/buttons.get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
 		
 
 func _on_Button_pressed(scene_to_load):
 	scene_path_to_load = scene_to_load
-	get_tree().change_scene(scene_path_to_load);
-#	$FadeIn.show()
-#	$FadeIn.fade_in()
+	$FadeIn.show()
+	$FadeIn.fade_in()
 
-#func _on_FadeIn_fade_finished():
-#	print(scene_path_to_load)
-#	if scene_path_to_load == "Exit":
-#		get_tree().quit()
-#	else:
-#		get_tree().change_scene(scene_path_to_load)
-#
-#
-#func _on_FadeOut_fade_finished():
-#	$FadeOut.queue_free()
+func _on_FadeIn_fade_finished():
+	if scene_path_to_load == "Exit":
+		get_tree().quit()
+	else:
+		get_tree().change_scene(scene_path_to_load)
+
+func _on_FadeOut_fade_finished():
+	$FadeOut.queue_free()
