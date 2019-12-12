25d1cbde5962a2ecb2383ac35ae80c2386a0f40e
Pyrozen <zeropyrozen@gmail.com>
2019-12-12 16:43:34 +0700

    Disable Fade Out Transition and Change Back Button for Pause Menu

--- senterin_mobile/menu/main_menu.gd
+++ senterin_mobile/menu/main_menu.gd
@@ -4,8 +4,6 @@ extends Control
 var scene_path_to_load
 func _ready():
 	# Test
-	$FadeOut.show()
-	$FadeOut.fade_out()
 	for button in $menu/center_row/buttons.get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
 		
@@ -21,5 +19,3 @@ func _on_FadeIn_fade_finished():
 	else:
 		get_tree().change_scene(scene_path_to_load)
 
-func _on_FadeOut_fade_finished():
-	$FadeOut.queue_free()
--- senterin_mobile/menu/pause_menu.gd
+++ senterin_mobile/menu/pause_menu.gd
@@ -2,8 +2,7 @@ extends Control
 
 var scene_path_to_load
 func _ready():
-	$FadeOut.show()
-	$FadeOut.fade_out()
+
 	for button in $menu/centre_row/buttons.get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
 		
@@ -15,7 +14,3 @@ func _on_Button_pressed(scene_to_load):
 
 func _on_FadeIn_fade_finished():
 	get_tree().change_scene(scene_path_to_load)
-
-func _on_FadeOut_fade_finished():
-	$FadeOut.queue_free()
-
