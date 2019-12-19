c1a1ca7a45da58ba2adea55a1920f13f3c182456
ZeroPyrozen <zeropyrozen@gmail.com>
2019-12-19 16:36:55 +0700

    Change Scene Load from Direct Link to PackedScene Class

--- senterin_mobile/menu/main_menu.gd
+++ senterin_mobile/menu/main_menu.gd
@@ -1,25 +1,20 @@
 extends Control
+export var scene_to_load: PackedScene
 
-
-var scene_path_to_load
 func _ready():
 	#Test
 	$FadeOut.show()
 	$FadeOut.fade_out()
 	for button in $menu/center_row/buttons.get_children():
-		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+		button.connect("pressed", self, "_on_Button_pressed")
 		
 
-func _on_Button_pressed(scene_to_load):
-	scene_path_to_load = scene_to_load
+func _on_Button_pressed():
 	$FadeIn.show()
 	$FadeIn.fade_in()
 
 func _on_FadeIn_fade_finished():
-	if scene_path_to_load == "Exit":
-		get_tree().quit()
-	else:
-		get_tree().change_scene(scene_path_to_load)
+	get_tree().change_scene_to(scene_to_load)
 
 func _on_FadeOut_fade_finished():
 	$FadeOut.queue_free()
--- senterin_mobile/menu/pause_menu.gd
+++ senterin_mobile/menu/pause_menu.gd
@@ -1,16 +1,14 @@
 extends Control
 
-var scene_path_to_load
+export var scene_to_load: PackedScene
 func _ready():
-
 	for button in $menu/centre_row/buttons.get_children():
-		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+		button.connect("pressed", self, "_on_Button_pressed")
 		
 
-func _on_Button_pressed(scene_to_load):
-	scene_path_to_load = scene_to_load
+func _on_Button_pressed():
 	$FadeIn.show()
 	$FadeIn.fade_in()
 
 func _on_FadeIn_fade_finished():
-	get_tree().change_scene(scene_path_to_load)
+	get_tree().change_scene_to(scene_to_load)
