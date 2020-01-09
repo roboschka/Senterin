d17ee922fd481e09048d14d69149e68edee4d7cf
Pyrozen <william.oky@binus.ac.id>
2020-01-09 16:10:54 +0700

    Add Door

--- senterin_mobile/map_element/door.gd
+++ senterin_mobile/map_element/door.gd
@@ -4,4 +4,10 @@ export var destination:PackedScene
 
 func _on_door_body_entered(body):
 	if body.is_in_group("player"):
-		get_tree().change_scene_to(destination)
+		$ColorRect.show()
+		$ColorRect.fade_in()
+		
+
+
+func _on_ColorRect_fade_finished():
+	get_tree().change_scene_to(destination)
